sanchitrohilla/abc_frameworks_base

Allow to disable qs footer warnings [1/2]
When using apps like AdGuard that create a vpn, a warning is displayed
in the QS panel footer, so the user knows the device net is being monitored
by an external app.
The warning takes space and the user might want to avoid it if he already
knows that the app is running (also because there's a statusbar icon
for the same purpose)

packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -582,4 +582,10 @@ public class QSPanel extends LinearLayout implements Tunable, Callback {
 
         void setListening(boolean listening);
     }
+
+    public void updateSettings() {
+        if (mFooter != null) {
+            mFooter.updateSettings();
+        }
+    }
 }

aex-tmp/platform_frameworks_base

Change-Id: I909cacc67aa44f62489068d3903b7a24e94ea18a
Signed-off-by: Shubham Singh <coolsks94@gmail.com>
Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -238,6 +238,7 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
 
     protected void addSecurityFooter() {
         mSecurityFooter = new QSSecurityFooter(this, mContext);
+        mSecurityFooter.updateSettings();
     }
 
     protected void addViewsAboveTiles() {
@@ -255,7 +256,6 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
         }
         return mRegularTileLayout;
     }
-
 
     protected QSTileLayout createHorizontalTileLayout() {
         return createRegularTileLayout();
@@ -1315,6 +1315,9 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
             for (TileRecord r : mRecords) {
                 configureTile(r.tile, r.tileView);
             }
+        }
+        if (mSecurityFooter != null) {
+            mSecurityFooter.updateSettings();
         }
     }
 