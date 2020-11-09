Havoc-OS/android_frameworks_base

base: Bring back Gaming mode [1/3]
This is a squash commit with major contributions by
@PMS22, @ishubhamsingh, @DennySPB, @neobuddy89

@SKULSHADY: Adapt for SettingsObserver and update tile for Android 10

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

packages/SystemUI/src/com/android/systemui/qs/tiles/GamingModeTile.java
@@ -1,0 +1,128 @@
+/*
+ * Copyright (C) 2019 crDroidAndroid Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.service.quicksettings.Tile;
+import android.widget.Toast;
+
+import com.android.systemui.R;
+import com.android.systemui.SysUIToast;
+import com.android.systemui.plugins.qs.QSTile.BooleanState;
+import com.android.systemui.qs.QSHost;
+import com.android.systemui.qs.SystemSetting;
+import com.android.systemui.qs.tileimpl.QSTileImpl;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+
+import javax.inject.Inject;
+
+public class GamingModeTile extends QSTileImpl<BooleanState> {
+
+    private final SystemSetting mGamingModeActivated;
+    private final Icon mIcon = ResourceIcon.get(R.drawable.ic_qs_gaming_mode);
+    private static final Intent GAMING_MODE_SETTINGS = new Intent("android.settings.GAMING_MODE_SETTINGS");
+
+    @Inject
+    public GamingModeTile(QSHost host) {
+        super(host);
+        mGamingModeActivated = new SystemSetting(mContext, mHandler, Settings.System.GAMING_MODE_ACTIVE) {
+            @Override
+            protected void handleValueChanged(int value, boolean observedChange) {
+                handleRefreshState(value);
+            }
+        };
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public BooleanState newTileState() {
+        return new BooleanState();
+    }
+
+    @Override
+    protected void handleClick() {
+        boolean gamingModeEnabled = Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.GAMING_MODE_ENABLED, 0) == 1;
+        mHost.collapsePanels();
+        if (gamingModeEnabled) {
+            mGamingModeActivated.setValue(mState.value ? 0 : 1);
+        } else {
+            SysUIToast.makeText(mContext, mContext.getString(
+                    R.string.gaming_mode_not_enabled),
+                    Toast.LENGTH_LONG).show();
+        }
+        refreshState();
+    }
+
+    @Override
+    public Intent getLongClickIntent() {
+        return GAMING_MODE_SETTINGS;
+    }
+
+    @Override
+    public CharSequence getTileLabel() {
+        return mContext.getString(R.string.quick_settings_gaming_mode_label);
+    }
+
+    @Override
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        final int value = arg instanceof Integer ? (Integer)arg : mGamingModeActivated.getValue();
+        final boolean enable = value == 1;
+        state.value = enable;
+        state.label = mContext.getString(R.string.quick_settings_gaming_mode_label);
+        state.icon = mIcon;
+        if (enable) {
+            state.contentDescription =  mContext.getString(
+                    R.string.accessibility_quick_settings_gaming_mode_enabled);
+            state.state = Tile.STATE_ACTIVE;
+        } else {
+            state.contentDescription =  mContext.getString(
+                    R.string.accessibility_quick_settings_gaming_mode_disabled);
+            state.state = Tile.STATE_INACTIVE;
+        }
+    }
+
+    @Override
+    protected String composeChangeAnnouncement() {
+        if (mState.value) {
+            return mContext.getString(
+                    R.string.accessibility_quick_settings_gaming_mode_enabled);
+        } else {
+            return mContext.getString(
+                    R.string.accessibility_quick_settings_gaming_mode_disabled);
+        }
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.HAVOC_SETTINGS;
+    }
+
+    @Override
+    public void handleSetListening(boolean listening) {
+        // Do nothing
+    }
+}

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -473,6 +473,8 @@ public class StatusBar extends SystemUI implements DemoMode,
 
     private PackageMonitor mPackageMonitor;
 
+    private boolean mHeadsUpDisabled, mGamingModeActivated;
+
     // XXX: gesture research
     private final GestureRecorder mGestureRec = DEBUG_GESTURES
         ? new GestureRecorder("/sdcard/statusbar_gestures.dat")
@@ -4056,6 +4058,12 @@ public class StatusBar extends SystemUI implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.GAMING_MODE_ACTIVE),
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.GAMING_MODE_HEADSUP_TOGGLE),
+                    false, this, UserHandle.USER_ALL);
         }
 
         @Override
@@ -4072,6 +4080,7 @@ public class StatusBar extends SystemUI implements DemoMode,
             setPulseOnNewTracks();
             setUseLessBoringHeadsUp();
             setFpToDismissNotifications();
+            setGamingMode();
         }
     }
 
@@ -4132,6 +4141,16 @@ public class StatusBar extends SystemUI implements DemoMode,
         mFpDismissNotifications = Settings.Secure.getIntForUser(mContext.getContentResolver(),
                 Settings.Secure.FP_SWIPE_TO_DISMISS_NOTIFICATIONS, 0,
                 UserHandle.USER_CURRENT) == 1;
+    }
+
+    private void setGamingMode() {
+        mGamingModeActivated = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.GAMING_MODE_ACTIVE, 0,
+                UserHandle.USER_CURRENT) == 1;
+        mHeadsUpDisabled = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.GAMING_MODE_HEADSUP_TOGGLE, 1,
+                UserHandle.USER_CURRENT) == 1;
+        mNotificationInterruptionStateProvider.setGamingPeekMode(mGamingModeActivated && mHeadsUpDisabled);
     }
 
     public int getWakefulnessState() {

aex-tmp/platform_frameworks_base

Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
Change-Id: I56a3150cab94949e5220c3c48e89cc80fff699c7

packages/SystemUI/src/com/android/systemui/qs/tiles/GamingModeTile.java
@@ -1,0 +1,128 @@
+/*
+ * Copyright (C) 2019 crDroidAndroid Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.service.quicksettings.Tile;
+import android.widget.Toast;
+
+import com.android.systemui.R;
+import com.android.systemui.SysUIToast;
+import com.android.systemui.plugins.qs.QSTile.BooleanState;
+import com.android.systemui.qs.QSHost;
+import com.android.systemui.qs.SystemSetting;
+import com.android.systemui.qs.tileimpl.QSTileImpl;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+
+import javax.inject.Inject;
+
+public class GamingModeTile extends QSTileImpl<BooleanState> {
+
+    private final SystemSetting mGamingModeActivated;
+    private final Icon mIcon = ResourceIcon.get(R.drawable.ic_qs_gaming_mode);
+    private static final Intent GAMING_MODE_SETTINGS = new Intent("android.settings.GAMING_MODE_SETTINGS");
+
+    @Inject
+    public GamingModeTile(QSHost host) {
+        super(host);
+        mGamingModeActivated = new SystemSetting(mContext, mHandler, Settings.System.GAMING_MODE_ACTIVE) {
+            @Override
+            protected void handleValueChanged(int value, boolean observedChange) {
+                handleRefreshState(value);
+            }
+        };
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public BooleanState newTileState() {
+        return new BooleanState();
+    }
+
+    @Override
+    protected void handleClick() {
+        boolean gamingModeEnabled = Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.GAMING_MODE_ENABLED, 0) == 1;
+        mHost.collapsePanels();
+        if (gamingModeEnabled) {
+            mGamingModeActivated.setValue(mState.value ? 0 : 1);
+        } else {
+            SysUIToast.makeText(mContext, mContext.getString(
+                    R.string.gaming_mode_not_enabled),
+                    Toast.LENGTH_LONG).show();
+        }
+        refreshState();
+    }
+
+    @Override
+    public Intent getLongClickIntent() {
+        return GAMING_MODE_SETTINGS;
+    }
+
+    @Override
+    public CharSequence getTileLabel() {
+        return mContext.getString(R.string.quick_settings_gaming_mode_label);
+    }
+
+    @Override
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        final int value = arg instanceof Integer ? (Integer)arg : mGamingModeActivated.getValue();
+        final boolean enable = value == 1;
+        state.value = enable;
+        state.label = mContext.getString(R.string.quick_settings_gaming_mode_label);
+        state.icon = mIcon;
+        if (enable) {
+            state.contentDescription =  mContext.getString(
+                    R.string.accessibility_quick_settings_gaming_mode_enabled);
+            state.state = Tile.STATE_ACTIVE;
+        } else {
+            state.contentDescription =  mContext.getString(
+                    R.string.accessibility_quick_settings_gaming_mode_disabled);
+            state.state = Tile.STATE_INACTIVE;
+        }
+    }
+
+    @Override
+    protected String composeChangeAnnouncement() {
+        if (mState.value) {
+            return mContext.getString(
+                    R.string.accessibility_quick_settings_gaming_mode_enabled);
+        } else {
+            return mContext.getString(
+                    R.string.accessibility_quick_settings_gaming_mode_disabled);
+        }
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.EXTENSIONS;
+    }
+
+    @Override
+    public void handleSetListening(boolean listening) {
+        // Do nothing
+    }
+}

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -481,6 +481,8 @@ public class StatusBar extends SystemUI implements DemoMode,
 
     private PackageMonitor mPackageMonitor;
 
+    private boolean mHeadsUpDisabled, mGamingModeActivated;
+
     // XXX: gesture research
     private final GestureRecorder mGestureRec = DEBUG_GESTURES
         ? new GestureRecorder("/sdcard/statusbar_gestures.dat")
@@ -4440,6 +4442,12 @@ public class StatusBar extends SystemUI implements DemoMode,
                     false, this, UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.FORCE_SHOW_NAVBAR),
+                   false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.GAMING_MODE_ACTIVE),
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.GAMING_MODE_HEADSUP_TOGGLE),
                     false, this, UserHandle.USER_ALL);
         }
 
@@ -4482,6 +4490,7 @@ public class StatusBar extends SystemUI implements DemoMode,
             setScreenBrightnessMode();
             setUseLessBoringHeadsUp();
             updateNavigationBarVisibility();
+            setGamingMode();
         }
     }
 
@@ -4572,6 +4581,16 @@ public class StatusBar extends SystemUI implements DemoMode,
         }
     }
 }
+
+    private void setGamingMode() {
+        mGamingModeActivated = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.GAMING_MODE_ACTIVE, 0,
+                UserHandle.USER_CURRENT) == 1;
+        mHeadsUpDisabled = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.GAMING_MODE_HEADSUP_TOGGLE, 1,
+                UserHandle.USER_CURRENT) == 1;
+        mNotificationInterruptStateProvider.setGamingPeekMode(mGamingModeActivated && mHeadsUpDisabled);
+    }
 
     private final BroadcastReceiver mBannerActionBroadcastReceiver = new BroadcastReceiver() {
         @Override