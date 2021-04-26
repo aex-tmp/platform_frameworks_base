aex-tmp/platform_frameworks_base

commit 28fbc82a275145c7b51641029c15d6d6d9491303

Lockscreen MediaArt: hide if media is not playing
it's annoying to get the artwork even if the notification gets
swiped out and media is paused, because the media session is still alive

Change-Id: I3540e1429524d34029d273c5cbe216fa46e4d59e

packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
@@ -609,7 +609,9 @@ public class NotificationMediaManager implements Dumpable, TunerService.Tunable 
             mScrimController.setHasBackdrop(hasArtwork);
         }
 
-        if ((hasArtwork || DEBUG_MEDIA_FAKE_ARTWORK)
+        // show artwork only if the media is playing
+        if (PlaybackState.STATE_PLAYING == getMediaControllerPlaybackState(mMediaController)
+                && (hasArtwork || DEBUG_MEDIA_FAKE_ARTWORK)
                 && (mStatusBarStateController.getState() != StatusBarState.SHADE || allowWhenShade)
                 &&  mBiometricUnlockController != null && mBiometricUnlockController.getMode()
                         != BiometricUnlockController.MODE_WAKE_AND_UNLOCK_PULSING

commit 1233c995b1f2f2a0ddbc4e312bb99b267175264f

[SQUASH][fixup!]: Lockscreen media art upstreamed
Revert "Fix lockscreen wallpaper not being set if different from the Home one"

This reverts commit fcb89e7dbe05270193223a9ad40463706ce427f6.

Revert "Lockscreen MediaArt: hide if media is not playing"

This reverts commit f168fe739ce89bbc24515c2f48d6dc7b8419d8df.

Revert "Allow to disable lockscreen media artwork"

This reverts commit f4fa39ffff522dda4e99254614fd6526cf361c29.

Change-Id: Ie2e11c4e66edde71a08b70669a981222ea8a3b2b

SystemUI: Allow to enable Lockscreen Media Art [1/2]

Change-Id: Id93e115649df5d6dc680101a8c852644b217c28a

Lockscreen MediaArt: hide if media is not playing

it's annoying to get the artwork even if the notification gets
swiped out and media is paused, because the media session is still alive

Change-Id: I3540e1429524d34029d273c5cbe216fa46e4d59e

Fix lockscreen wallpaper not being set if different from the Home one

this got broken with:
https://github.com/ezio84/abc_frameworks_base/commit/28fbc82a275145c7b51641029c15d6d6d9491303

thanks to @anayw2001 who digged into commits chain and found
the root of the issue

Change-Id: Ifc96f6e540897a43fdd3cf23c8ac518a7f2f77e2
Signed-off-by: Raj Jadeja <rajsjadeja4@gmail.com>

core/java/android/provider/Settings.java
@@ -5264,12 +5264,6 @@ public final class Settings {
         public static final String LOCKSCREEN_MEDIA_BLUR = "lockscreen_media_blur";
 
         /**
-         * media artwork wallpaper blur level on lockscreen
-         * @hide
-         */
-        public static final String SHOW_LOCKSCREEN_MEDIA_ART = "show_lockscreen_media_art";
-
-        /**
          * whether to enable or disable vibration on succesful fingerprint auth
          *
          * @hide
@@ -10199,6 +10193,11 @@ public final class Settings {
          */
         public static final String NAVIGATION_BAR_HINT = "navigation_bar_hint";
         
+        /** Whether to show lockscreen media art
+         *
+         * @hide
+         */
+        public static final String LOCKSCREEN_MEDIA_METADATA = "lockscreen_media_metadata";
     }
 
     /**

packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
@@ -75,6 +75,7 @@ import com.android.systemui.util.Utils;
 import com.android.systemui.util.concurrency.DelayableExecutor;
 import com.android.systemui.statusbar.phone.StatusBar;
 import com.android.systemui.statusbar.VisualizerView;
+import com.android.systemui.tuner.TunerService;
 
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
@@ -91,9 +92,12 @@ import dagger.Lazy;
  * Handles tasks and state related to media notifications. For example, there is a 'current' media
  * notification, which this class keeps track of.
  */
-public class NotificationMediaManager implements Dumpable {
+public class NotificationMediaManager implements Dumpable, TunerService.Tunable {
     private static final String TAG = "NotificationMediaManager";
     public static final boolean DEBUG_MEDIA = false;
+
+    private static final String LOCKSCREEN_MEDIA_METADATA =
+            Settings.Secure.LOCKSCREEN_MEDIA_METADATA;
 
     private final StatusBarStateController mStatusBarStateController
             = Dependency.get(StatusBarStateController.class);
@@ -126,7 +130,6 @@ public class NotificationMediaManager implements Dumpable {
     private final DelayableExecutor mMainExecutor;
 
     private float mLockscreenMediaBlur;
-    private boolean mShowLockscreenMediaArt;
 
     private final Context mContext;
     private final MediaSessionManager mMediaSessionManager;
@@ -145,6 +148,8 @@ public class NotificationMediaManager implements Dumpable {
     private ImageView mBackdropBack;
 
     private boolean mShowCompactMediaSeekbar;
+    private boolean mShowMediaMetadata;
+
     private final DeviceConfig.OnPropertiesChangedListener mPropertiesChangedListener =
             new DeviceConfig.OnPropertiesChangedListener() {
         @Override
@@ -288,6 +293,17 @@ public class NotificationMediaManager implements Dumpable {
         deviceConfig.addOnPropertiesChangedListener(DeviceConfig.NAMESPACE_SYSTEMUI,
                 mContext.getMainExecutor(),
                 mPropertiesChangedListener);
+
+        final TunerService tunerService = Dependency.get(TunerService.class);
+        tunerService.addTunable(this, LOCKSCREEN_MEDIA_METADATA);
+    }
+
+    @Override
+    public void onTuningChanged(String key, String newValue) {
+        if (LOCKSCREEN_MEDIA_METADATA.equals(key)) {
+            mShowMediaMetadata = TunerService.parseIntegerSwitch(newValue, false);
+            dispatchUpdateMediaMetaData(false /* changed */, true /* allowAnimation */);
+        }
     }
 
     private void removeEntry(NotificationEntry entry) {
@@ -551,7 +567,7 @@ public class NotificationMediaManager implements Dumpable {
         }
 
         Bitmap artworkBitmap = null;
-        if (mShowLockscreenMediaArt && mediaMetadata != null && !mKeyguardBypassController.getBypassEnabled()) {
+        if (mediaMetadata != null && !mKeyguardBypassController.getBypassEnabled()) {
             artworkBitmap = mediaMetadata.getBitmap(MediaMetadata.METADATA_KEY_ART);
             if (artworkBitmap == null) {
                 artworkBitmap = mediaMetadata.getBitmap(MediaMetadata.METADATA_KEY_ALBUM_ART);
@@ -579,11 +595,14 @@ public class NotificationMediaManager implements Dumpable {
     private void finishUpdateMediaMetaData(boolean metaDataChanged, boolean allowEnterAnimation,
             @Nullable Bitmap bmp) {
         Drawable artworkDrawable = null;
-        if (bmp != null && PlaybackState.STATE_PLAYING == getMediaControllerPlaybackState(mMediaController)) {
+        // set media artwork as lockscreen wallpaper if player is playing
+        if (bmp != null && (mShowMediaMetadata || !ENABLE_LOCKSCREEN_WALLPAPER) &&
+                PlaybackState.STATE_PLAYING == getMediaControllerPlaybackState(mMediaController)) {
             artworkDrawable = new BitmapDrawable(mBackdropBack.getResources(), bmp);
         }
         boolean hasMediaArtwork = artworkDrawable != null;
         boolean allowWhenShade = false;
+        // if no media artwork, show normal lockscreen wallpaper
         if (ENABLE_LOCKSCREEN_WALLPAPER && artworkDrawable == null) {
             Bitmap lockWallpaper =
                     mLockscreenWallpaper != null ? mLockscreenWallpaper.getBitmap() : null;
@@ -765,12 +784,6 @@ public class NotificationMediaManager implements Dumpable {
         mLockscreenMediaBlur = (float) Settings.System.getIntForUser(mContext.getContentResolver(),
                 Settings.System.LOCKSCREEN_MEDIA_BLUR, 25,
                 UserHandle.USER_CURRENT);
-    }
-
-    public void setLockScreenMediaArt() {
-        mShowLockscreenMediaArt = Settings.System.getIntForUser(mContext.getContentResolver(),
-                Settings.System.SHOW_LOCKSCREEN_MEDIA_ART, 1,
-                UserHandle.USER_CURRENT) == 1;
     }
 
     @MainThread

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -4494,9 +4494,6 @@ public class StatusBar extends SystemUI implements DemoMode,
                     Settings.System.LOCKSCREEN_MEDIA_BLUR),
                     false, this, UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.SHOW_LOCKSCREEN_MEDIA_ART),
-                    false, this, UserHandle.USER_ALL);
-            resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.QS_FOOTER_WARNINGS),
                     false, this, UserHandle.USER_ALL);
             /*resolver.registerContentObserver(Settings.System.getUriFor(
@@ -4547,9 +4544,6 @@ public class StatusBar extends SystemUI implements DemoMode,
                     Settings.System.LOCKSCREEN_MEDIA_BLUR))) {
                 setLockScreenMediaBlurLevel();
             } else if (uri.equals(Settings.System.getUriFor(
-                    Settings.System.SHOW_LOCKSCREEN_MEDIA_ART))) {
-                setLockScreenMediaArt();
-            } else if (uri.equals(Settings.System.getUriFor(
                     Settings.System.QS_FOOTER_WARNINGS))) {
                 setQsPanelOptions();
             } else if (uri.equals(Settings.System.getUriFor(
@@ -4571,7 +4565,6 @@ public class StatusBar extends SystemUI implements DemoMode,
             setHeadsUpBlacklist();
             setStatusBarWindowViewOptions();
             setLockScreenMediaBlurLevel();
-            setLockScreenMediaArt();
             setQsPanelOptions();
             setScreenBrightnessMode();
             setUseLessBoringHeadsUp();
@@ -4647,12 +4640,6 @@ public class StatusBar extends SystemUI implements DemoMode,
     private void setLockScreenMediaBlurLevel() {
         if (mMediaManager != null) {
             mMediaManager.setLockScreenMediaBlurLevel();
-        }
-    }
-
-    private void setLockScreenMediaArt() {
-        if (mMediaManager != null) {
-            mMediaManager.setLockScreenMediaArt();
         }
     }
 